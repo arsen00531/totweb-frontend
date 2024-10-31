8a58f8f47053b90611429788abf2adc7a0c7e79f
arsen00531 <arsendag505@gmail.com>
2024-10-31 13:47:02 +0300

    delete styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import './App.css'
 import Registration from './pages/Registration'
-// import { useCounterZustand } from './store/zustand.store'
 
 function App() {
-
-  // const { name } = useCounterZustand()
 
   return (
     <>
